7f5f0ea80e3cff9abfb05acc564240b6753fe097
TheMesp <webd@mts.net>
2016-08-19 23:12:11 -0500

    Adjusted layers. Got started on TileSysttem class.

---
+++ Optic Coma/Optic Coma/Types/TileSystem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+namespace Optic_Coma.Types
+{
+    public class TileSystem
+    {
+        public TileSystem(Texture2D texture)
+        {
+
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
