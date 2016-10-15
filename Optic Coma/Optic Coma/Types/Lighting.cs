cb8424017e99f5e4815e8d3e04a1f2d50921a267
Michael <Michael@MICHAEL>
2016-10-14 20:22:00 -0500

    Introduced Lighting interface... now researching for how to turn this into a real engine. Interfaces may be very useful for implementing buttons too, i might try doing that

---
+++ Optic Coma/Optic Coma/Types/Lighting.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optic_Coma
+{
+    // Considering using an interface for lighting
+    // What this does is allow you to call a new instance of "Lighting
+    // Then implement the methods and variables you need without having to go through the lines
+    // It also lets you change pretty much anything you need
+    // Since classes can only inherit from one other class at a time, this allows you to implement multiple features
+    // Between classes without having to rely on polymorphism and inheritance
+    // Naming convention for interfaces: interface IName<T>
+    public interface ILighting<T>
+    {
+        // This is the "signature" not sure what its uses is yet... researching
+        bool Equals(T obj);
+    }
+}
